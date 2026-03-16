aa3722a990f9418273390357777dd454a60ad5ed
vytrannguyenthao <tvytran4642@gmail.com>
2026-03-16 20:32:44 +0700

    App/robot: Add pwm testcase

--- App/HIL_robot/libraries/hil_lib.py
+++ App/HIL_robot/libraries/hil_lib.py
@@ -2,9 +2,6 @@ from robot.api.deco import library, keyword
 from robot.api import logger
 from cli_lib import CLI
 import re
-from robot.api import logger
-from robot.api.deco import keyword
-from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
 
 @library(scope="GLOBAL")
@@ -65,3 +62,51 @@ class HILLibrary:
 
         return resp
     
+    # ------------------
+    # Power control
+    # ------------------
+    @keyword("Power DUT On")
+    def power_dut_on(self):
+        resp = self.cli.execute("dut_power 1")
+        logger.info(resp)
+
+    # ------------------
+    # PWM
+    # ------------------
+    @keyword("Read HIL Voltage")
+    def read_voltage(self):
+
+        resp = self.cli.execute("adc_read_pwm")
+
+        match = re.search(r"Measured:\s*(\d+)\s*mV", resp)
+        if not match:
+            raise AssertionError("ADC value not found")
+
+        voltage = int(match.group(1)) / 1000.0
+        logger.info(f"Voltage = {voltage} V")
+
+        return voltage
+    
+    @keyword("Voltage Should Be In Range")
+    def voltage_should_be_in_range(self, min_v, max_v):
+
+        voltage = self.read_voltage()
+
+        if not (float(min_v) <= voltage <= float(max_v)):
+            raise AssertionError(
+                f"Voltage out of range: {voltage}V"
+            )
+    
+    @keyword("Wait Until Voltage Stable")
+    def wait_until_voltage_stable(self, min_v, max_v):
+        """
+        Wait until ADC voltage becomes stable.
+        """
+        BuiltIn().run_keyword(
+            "Wait Until Keyword Succeeds",
+            "5s",
+            "200ms",
+            "Voltage Should Be In Range",
+            min_v,
+            max_v,
+        )
