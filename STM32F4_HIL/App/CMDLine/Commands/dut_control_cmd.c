f4a924074c4843fcf33f32ea9e1dc9bca3c17016
vytrannguyenthao <tvytran4642@gmail.com>
2026-03-22 14:22:35 +0700

    STM32F4_HIL: Add set DUT BOOT0 command

--- STM32F4_HIL/App/CMDLine/Commands/dut_control_cmd.c
+++ STM32F4_HIL/App/CMDLine/Commands/dut_control_cmd.c
@@ -2,7 +2,7 @@
  * dut_control_cmd.c
  *
  *  Created on: Mar 16, 2026
- *      Author: Admin
+ *      Author: Vy Tran
  */
 
 #ifndef CMDLINE_COMMANDS_DUT_CONTROL_CMD_C_
@@ -15,11 +15,10 @@
 #include "FreeRTOS.h"
 #include "w25q_slave.h"
 
-static int Cmd_Power_DUT(int argc, char *argv[]) {
-    if (argc < 3)
-        return CMDLINE_TOO_FEW_ARGS;
-    if (argc > 3)
-        return CMDLINE_TOO_MANY_ARGS;
+static int Cmd_Power_DUT(int argc, char *argv[])
+{
+	if (argc != 3)
+	    return (argc < 3) ? CMDLINE_TOO_FEW_ARGS : CMDLINE_TOO_MANY_ARGS;
 
     uint32_t status = atoi(argv[1]);
     if(status) {
@@ -51,7 +50,8 @@ static int Cmd_Power_DUT(int argc, char *argv[]) {
     return CMDLINE_OK;
 }
 
-static int Cmd_Power_Status(int argc, char *argv[]) {
+static int Cmd_Power_Status(int argc, char *argv[])
+{
     if (argc > 2)
         return CMDLINE_TOO_MANY_ARGS;
 
@@ -66,10 +66,49 @@ static int Cmd_Power_Status(int argc, char *argv[]) {
     return CMDLINE_OK;
 }
 
+static int Cmd_Set_BOOT0(int argc, char *argv[])
+{
+	if (argc != 3)
+	    return (argc < 3) ? CMDLINE_TOO_FEW_ARGS : CMDLINE_TOO_MANY_ARGS;
+
+    uint32_t status = atoi(argv[1]);
+    switch (status) {
+		case 0:
+	        LL_GPIO_ResetOutputPin(DUT_BOOT0_GPIO_Port, DUT_BOOT0_Pin);
+
+	        vTaskDelay(pdMS_TO_TICKS(100));
+
+	        if (!LL_GPIO_IsOutputPinSet(DUT_BOOT0_GPIO_Port, DUT_BOOT0_Pin)) {
+	        	Console_Write("DUT BOOT0 set to low successfully \r\n");
+	        } else {
+	        	Console_Write("FAIL to set DUT BOOT0 to low \r\n");
+	        }
+
+			break;
+		case 1:
+	        LL_GPIO_SetOutputPin(DUT_BOOT0_GPIO_Port, DUT_BOOT0_Pin);
+
+	        vTaskDelay(pdMS_TO_TICKS(100));
+
+	        if (LL_GPIO_IsOutputPinSet(DUT_BOOT0_GPIO_Port, DUT_BOOT0_Pin)) {
+	        	Console_Write("DUT BOOT0 set to high successfully \r\n");
+	        } else {
+	        	Console_Write("FAIL to set DUT BOOT0 to high \r\n");
+	        }
+
+			break;
+		default:
+			return CMDLINE_INVALID_ARG;
+	}
+
+	return CMDLINE_OK;
+}
+
 void Cmd_DUT_Control_Register(void)
 {
 	CLI_RegisterCommand("dut_power", Cmd_Power_DUT, "Power DUT on/off | format: dut_power <0|1>");
 	CLI_RegisterCommand("dut_power_status", Cmd_Power_Status, "Check DUT power status | format: dut_power_status");
+	CLI_RegisterCommand("dut_boot0_set", Cmd_Set_BOOT0, "Set DUT BOOT0 status | format: dut_boot0_set <0|1>");
 }
 
 #endif /* CMDLINE_COMMANDS_DUT_CONTROL_CMD_C_ */
