431bb9a9f7468bbf04c4f7f2c1c47202038a7163
vesamy1234 <vesamy123@gmail.com>
2026-04-10 15:21:55 +0700

    STM32F4_HIL: Delete can_led

--- STM32F4_HIL/App/CMDLine/Commands/can_cmd.c
+++ STM32F4_HIL/App/CMDLine/Commands/can_cmd.c
@@ -47,50 +47,50 @@ static int Cmd_CAN_Read_Buffer(int argc, char *argv[]) {
 // 	return CMDLINE_OK;
 // }
 
-static int Cmd_CAN_LED(int argc, char *argv[]) {
-	if (argc < 4) {
-		return CMDLINE_TOO_FEW_ARGS;
-	}
-	if (argc > 4) {
-		return CMDLINE_TOO_MANY_ARGS;
-	}
+// static int Cmd_CAN_LED(int argc, char *argv[]) {
+// 	if (argc < 4) {
+// 		return CMDLINE_TOO_FEW_ARGS;
+// 	}
+// 	if (argc > 4) {
+// 		return CMDLINE_TOO_MANY_ARGS;
+// 	}
 
-	uint8_t tx_buf[2] = {0};
-	tx_buf[0] = atoi(argv[1]); // LED status
-	tx_buf[1] = atoi(argv[2]); // LED index
+// 	uint8_t tx_buf[2] = {0};
+// 	tx_buf[0] = atoi(argv[1]); // LED status
+// 	tx_buf[1] = atoi(argv[2]); // LED index
 
-	if (tx_buf[0] > 1) {
-		Console_Write("LED status must be 0 (OFF) or 1 (ON)\r\n");
-		return CMDLINE_OK;
-	}
+// 	if (tx_buf[0] > 1) {
+// 		Console_Write("LED status must be 0 (OFF) or 1 (ON)\r\n");
+// 		return CMDLINE_OK;
+// 	}
 
-	if (tx_buf[1] < 8 || tx_buf[1] > 10) {
-		Console_Write("LED index must be between 8 and 10\r\n");
-		return CMDLINE_OK;
-	}
+// 	if (tx_buf[1] < 8 || tx_buf[1] > 10) {
+// 		Console_Write("LED index must be between 8 and 10\r\n");
+// 		return CMDLINE_OK;
+// 	}
 
-	CAN_Send(tx_buf, 2);
+// 	CAN_Send(tx_buf, 2);
 
-	vTaskDelay(pdMS_TO_TICKS(10));
-	if (tx_buf[1] == 9) {
-		if (LL_GPIO_IsInputPinSet(GPIOD, LL_GPIO_PIN_9)) {
-			// LED_9 on
-			Console_Write("LED_9 ON\r\n");
-		} else {
-			// LED_9 off
-			Console_Write("LED_9 OFF\r\n");
-		}
-	} else if (tx_buf[1] == 10) {
-		if (LL_GPIO_IsInputPinSet(GPIOD, LL_GPIO_PIN_10)) {
-			// LED_10 on
-			Console_Write("LED_10 ON\r\n");
-		} else {
-			// LED_10 off
-			Console_Write("LED_10 OFF\r\n");
-		}
-	}
-	return CMDLINE_OK;
-}
+// 	vTaskDelay(pdMS_TO_TICKS(10));
+// 	if (tx_buf[1] == 9) {
+// 		if (LL_GPIO_IsInputPinSet(GPIOD, LL_GPIO_PIN_9)) {
+// 			// LED_9 on
+// 			Console_Write("LED_9 ON\r\n");
+// 		} else {
+// 			// LED_9 off
+// 			Console_Write("LED_9 OFF\r\n");
+// 		}
+// 	} else if (tx_buf[1] == 10) {
+// 		if (LL_GPIO_IsInputPinSet(GPIOD, LL_GPIO_PIN_10)) {
+// 			// LED_10 on
+// 			Console_Write("LED_10 ON\r\n");
+// 		} else {
+// 			// LED_10 off
+// 			Console_Write("LED_10 OFF\r\n");
+// 		}
+// 	}
+// 	return CMDLINE_OK;
+// }
 
 static int Cmd_CAN_Send_String(int argc, char *argv[])
 {
@@ -111,5 +111,5 @@ void Cmd_CAN_Register(void) {
 	CLI_RegisterCommand("can_send_string", Cmd_CAN_Send_String, "Send a string over CAN");
 	CLI_RegisterCommand("can_read", Cmd_CAN_Read_Buffer, "Read received CAN data");
 	// CLI_RegisterCommand("can_clear_buffer", Cmd_CAN_Clear_Buffer, "Clear received CAN data buffer");
-	CLI_RegisterCommand("can_led", Cmd_CAN_LED, "Turn on/ off DUT LED via CAN | format: can_led <led_status> <led_index>");
+	// CLI_RegisterCommand("can_led", Cmd_CAN_LED, "Turn on/ off DUT LED via CAN | format: can_led <led_status> <led_index>");
 }
